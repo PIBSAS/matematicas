6584ea3edfbc17f7de7e83a244b9996cb265974b
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-12-07 14:21:02 -0300

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -328,7 +328,7 @@ def generar_html(pdfs):
     <script>
     if ('serviceWorker' in navigator) {{
       window.addEventListener('load', function() {{
-        navigator.serviceWorker.register("pdfs/static/service-worker.js").then(function(registration) {{
+        navigator.serviceWorker.register("service-worker.js").then(function(registration) {{
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }}, function(err) {{
           console.log('ServiceWorker registration failed: ', err);
@@ -337,7 +337,7 @@ def generar_html(pdfs):
     }}
   </script>
     <div id="logo">
-        <img src="pdfs/static/logo.webp" alt="{folder_name}">
+        <img src="logo.webp" alt="{folder_name}">
     </div>
     <div class="pdfs-container">
 """
